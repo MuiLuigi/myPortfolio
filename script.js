3fb6a1616240da7885e1e11c9e55da773f813e64
Maruigi <fahadmui123@gmail.com>
2025-03-12 01:40:29 -0400

    Added basic CSS styling and responsive design

---
+++ script.js
@@ -0,0 +1,14 @@
+const descriptionButton = document.querySelectorAll(".description-btn");
+
+button.forEach(descriptionButton); {
+    button.addEventListener("click", function() {
+        const showDescription = showDescription.nextElementSibling;
+
+        if (description.style.display === "") {
+            description.style.display.textContent = "Hide";
+        }
+        else {
+            description.style.display.textContent = "Show Description";
+        }
+    })
+}
